a29698369be16b3f7709234acef654362739d6cd
Honza Javorek <mail@honzajavorek.cz>
2019-09-10 17:57:17 +0200

    implement redirects

--- charter/to_html.py
+++ charter/to_html.py
@@ -1,9 +1,1 @@
-#! /usr/bin/env python3
-
-import markdown
-
-with open('stanovy.txt') as f:
-    body = markdown.markdown(f.read())
-
-with open('template.html') as f:
-    print(f.read().replace('{{body}}', body))
+# přesunuto sem: https://docs.pyvec.org/operations/bylaws.html
